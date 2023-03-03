03a8555433d8647d9e561fda7642017e8dfb152f
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-03-03 13:44:27 +0530

    changes in table

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -11,53 +11,53 @@ const List = () => {
   const rows = [
     {
       id: 1143155,
-      product: "Acer Nitro 5",
-      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
+      product: "Hotel Linda",
+      img: "https://images.unsplash.com/photo-1625244695851-1fc873f942bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fGhvdGVsc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
       customer: "John Smith",
       date: "1 March",
       amount: 785,
-      method: "Cash on Delivery",
-      status: "Approved",
+      method: "Cash",
+      status: "Checked In",
     },
     {
       id: 2235235,
-      product: "Playstation 5",
-      img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
+      product: "Hotel Shamshera",
+      img: "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGhvdGVsc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
       customer: "Michael Doe",
       date: "1 March",
       amount: 900,
       method: "Online Payment",
-      status: "Pending",
+      status: "Checked In",
     },
     {
       id: 2342353,
-      product: "Redragon S101",
-      img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
+      product: "Hotel Gandhi",
+      img: "hhttps://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGhvdGVsc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
       customer: "John Smith",
       date: "1 March",
       amount: 35,
-      method: "Cash on Delivery",
-      status: "Pending",
+      method: "Cash",
+      status: "Checked In",
     },
     {
       id: 2357741,
-      product: "Razer Blade 15",
-      img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
+      product: "Hotel Razor",
+      img: "https://images.unsplash.com/photo-1596436889106-be35e843f974?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGhvdGVsc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
       customer: "Jane Smith",
       date: "1 March",
       amount: 920,
-      method: "Online",
-      status: "Approved",
+      method: "Cash",
+      status: "Checked In",
     },
     {
       id: 2342355,
-      product: "ASUS ROG Strix",
-      img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
+      product: "Hotel Seahag",
+      img: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdGVsc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
       customer: "Harold Carol",
       date: "1 March",
       amount: 2000,
-      method: "Online",
-      status: "Pending",
+      method: "Cash",
+      status: "Checked In",
     },
   ];
   return (
@@ -66,8 +66,8 @@ const List = () => {
         <TableHead>
           <TableRow>
             <TableCell className="tableCell">Tracking ID</TableCell>
-            <TableCell className="tableCell">Product</TableCell>
-            <TableCell className="tableCell">Customer</TableCell>
+            <TableCell className="tableCell">Hotel</TableCell>
+            <TableCell className="tableCell">Guest</TableCell>
             <TableCell className="tableCell">Date</TableCell>
             <TableCell className="tableCell">Amount</TableCell>
             <TableCell className="tableCell">Payment Method</TableCell>
